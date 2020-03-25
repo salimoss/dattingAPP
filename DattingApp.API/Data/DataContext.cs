a8d08cbfda1c2e5afc243002702b7ae72925c2f0
SALIM TAOUFIK <SALIM_TAOUFIK@neskdomain.local>
2020-03-26 00:54:37 +0100

    initial Commit

---
+++ DattingApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DattingApp.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DattingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {}
+        public DbSet<Value> values {get; set;}
+    }
+}
---
+++ DattingApp.API/Model/Value.cs
@@ -0,0 +1,8 @@
+namespace DattingApp.API.Model
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
